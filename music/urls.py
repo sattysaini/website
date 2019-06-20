c180531f03c5309d95998f37a509ede1ad700c61
satyam saini <stym.6996@gmail.com>
2019-06-20 21:46:16 +0530

    Views of albums

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    #/music/
     path('', views.index, name='index'),
+
+    #/music/<album_id>
+    path('<int:album_id>/', views.details, name='details')
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1> You can find the best Music here</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 
+def details(request, album_id):
+    return HttpResponse("<h1> Here is the Album " + str(album_id) + "</h1>")
