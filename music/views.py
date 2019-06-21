6d07d08bd00d6e859646daa9f0a9d37279c91fd0
satyam saini <stym.6996@gmail.com>
2019-06-21 20:39:21 +0530

    rendering templates

--- music/views.py
+++ music/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from .models import Album
-from django.template import loader
+from django.shortcuts import render
+#from django.template import loader
+
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums' : all_albums
-    }
-    return HttpResponse(template.render(context, request))
+    #template = loader.get_template('music/index.html')
+    context = { 'all_albums' : all_albums }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', context)
 
 
 def details(request, album_id):
